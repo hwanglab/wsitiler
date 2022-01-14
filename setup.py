1ce6533fd8c4dacf5feb8d77e0fae4d14e89cd26
Jean R. Clemenceau <jean.rene.clemenceau@gmail.com>
2022-01-14 11:26:45 -0500

    Fixed packages syntax error in setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
     #     "More Info": "https://github.com/hwanglab/xxxx"
     # },
     license='gpl-3.0',
-    packages=setuptools.find_packages(include=["wsitiler','wsitiler.*"]),
+    packages=setuptools.find_packages(include=['wsitiler','wsitiler.*']),
     install_requires=['numpy','pandas','matplotlib','scipy','scikit-image','openslide-python'],
 )
