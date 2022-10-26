266eda9f32119da296513fb90006067e3630cb1b
Jean R. Clemenceau <jean.rene.clemenceau@gmail.com>
2022-10-26 11:28:06 -0400

    Update version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='wsitiler',
-    version='0.0.2',
+    version='0.0.3',
     author='Jean R. Clemenceau',
     author_email='clemenceau.jean@mayo.com',
     description='Tools for dividing pathology whole slide images into tiles and save them as individual files.',
